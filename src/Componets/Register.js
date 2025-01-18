f5b94e4356a9e968b819fb9aea5f594796aea8d8
Tanaka9612 <u15057420@tuks.co.za>
2025-01-18 16:37:49 +0200

    Register page
    Fixed register page css

--- src/Componets/Register.js
+++ src/Componets/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import '../CSS/Register.css';
+import Login from "./Login";
 
 class Register extends React.Component{
     render(){
@@ -8,9 +9,24 @@ class Register extends React.Component{
             <div className="register">
                 <h3>Register</h3>
                 <form method="POST">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div id="username">
+                        <label>Email</label>
+                        <input type="email"/>
+                    </div>
+                    <div id="password">
+                        <label>Password</label>
+                        <input type="password"/>
+                    </div>
+                    <div id="password">
+                        <label>Confirm password</label>
+                        <input type="password"/>
+                    </div>
+                    <div id="login-text">
+                        <p id="already-member">Already a member? <Link to={"/login"} element={<Login/>}>Login here</Link></p>
+                    </div>
+                    <div id="button">
+                        <button type="submit">Register</button>
+                    </div>
                 </form>
             </div>
         )
